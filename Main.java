f0bc589ac1f0fb37c95fceaffca623ef73bc5ac7
Quentin <quentin.deharo@epfedu.fr>
2025-02-19 10:02:09 +0100

    TestGit2

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
        Table table = new Table();
         System.out.println("git");
+        System.out.println("git1");
+        System.out.println("git2");
     }
 }
